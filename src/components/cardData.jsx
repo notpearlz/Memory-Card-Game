093ba53045fed2bcb3677cf775beb22c746e7efa
Your Name <lolmebunbun@gmail.com>
2025-09-01 15:41:14 -0500

    add seenCards state

---
+++ src/components/cardData.jsx
@@ -0,0 +1,69 @@
+const cardData = [
+    {
+        id: 0,
+        val: "card0"
+    },
+    {
+        id: 1,
+        val: "card1"
+    },
+    {
+        id: 2,
+        val: "card2"
+    },
+    {
+        id: 3,
+        val: "card3"
+    },
+    {
+        id: 4,
+        val: "card4"
+    },
+    {
+        id: 5,
+        val: "card5"
+    },
+    {
+        id: 6,
+        val: "card6"
+    },
+    {
+        id: 7,
+        val: "card7"
+    },
+    {
+        id: 8,
+        val: "card8"
+    },
+    {
+        id: 9,
+        val: "card9"
+    },
+    {
+        id: 10,
+        val: "card10"
+    },
+    {
+        id: 11,
+        val: "card11"
+    },
+    {
+        id: 12,
+        val: "card12"
+    },
+    {
+        id: 13,
+        val: "card13"
+    },
+    {
+        id: 14,
+        val: "card14"
+    },
+    {
+        id: 15,
+        val: "card15"
+    }
+]
+
+
+export default cardData
--- src/components/game.jsx
+++ src/components/game.jsx
@@ -1,21 +1,60 @@
 import { useState, useEffect } from 'react'
+import cardData  from './cardData'
 
 import '../styles/game.css'
 
 function Game(){
-    const [cards, setCards] = useState([])
+    const [cards, setCards] = useState(cardData)
+    const [seenCards, setSeenCards] = useState(new Set())
 
 
-    useEffect(()=>{
-        setCards(Array(16).fill(null))
-    }, [])
+    function clickCard(card){
+        const cardId = card.target.dataset.id
+        var seen = false;
+        cards.map((x)=>{
+            if(x.id == cardId){
+                if(seenCards.has(cardId)){
+                    seen = true;
+                    return;
+                } else {
+                    setSeenCards(new Set([...seenCards, cardId]))
+                }
+            }
+        })
+
+        if(!seen){
+            shuffle();
+        } else {
+
+        }
+    }
+
+ 
+    function shuffle() {
+        let tempArr = [...cards];
+        let currentIndex = 0;
+
+
+        // While there remain elements to shuffle...
+        while (currentIndex != tempArr.length-1) {
+            // Pick a remaining element...
+            let randomIndex = Math.floor(Math.random() * currentIndex);
+            currentIndex++;
+
+            // And swap it with the current element.
+            [tempArr[currentIndex], tempArr[randomIndex]] = [
+            tempArr[randomIndex], tempArr[currentIndex]];
+        }
+
+        setCards(tempArr)
+    }
 
     return (
         <main>
         
-        {cards.map((val,index)=> {
-            return <button className='card' key={index}>Card{index}</button>
-        })}
+        {cards.map((card,index)=> {
+                return <button onClick={clickCard} className='card' data-id={card.id} key={card.id}>{card.val}</button>
+            })}
  
         </main>
     )
