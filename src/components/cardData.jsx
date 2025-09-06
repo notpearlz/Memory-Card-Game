a7fe869615393cc0566fe92afbf29b71ada9c4c6
Your Name <lolmebunbun@gmail.com>
2025-09-05 21:34:10 -0500

    Implement giphy api

--- src/components/cardData.jsx
+++ src/components/cardData.jsx
@@ -1,67 +1,83 @@
 const cardData = [
     {
         id: 0,
-        val: "card0"
+        val: "jotarokujo",
+        url: null
     },
     {
         id: 1,
-        val: "card1"
+        val: "jonathanjoestar",
+        url: null
     },
     {
         id: 2,
-        val: "card2"
+        val: "card2",
+        url: null
     },
     {
         id: 3,
-        val: "card3"
+        val: "card3",
+        url: null
     },
     {
         id: 4,
-        val: "card4"
+        val: "card4",
+        url: null
     },
     {
         id: 5,
-        val: "card5"
+        val: "card5",
+        url: null
     },
     {
         id: 6,
-        val: "card6"
+        val: "card6",
+        url: null
     },
     {
         id: 7,
-        val: "card7"
+        val: "card7",
+        url: null
     },
     {
         id: 8,
-        val: "card8"
+        val: "card8",
+        url: null
     },
     {
         id: 9,
-        val: "card9"
+        val: "card9",
+        url: null
     },
     {
         id: 10,
-        val: "card10"
+        val: "card10",
+        url: null
     },
     {
         id: 11,
-        val: "card11"
+        val: "card11",
+        url: null
     },
     {
         id: 12,
-        val: "card12"
+        val: "card12",
+        url: null
     },
     {
         id: 13,
-        val: "card13"
+        val: "card13",
+        url: null
     },
     {
         id: 14,
-        val: "card14"
+        val: "card14",
+        url: null
     },
     {
         id: 15,
-        val: "card15"
+        val: "card15",
+        url: null
     }
 ]
 
--- src/components/game.jsx
+++ src/components/game.jsx
@@ -4,9 +4,33 @@ import cardData  from './cardData'
 import '../styles/game.css'
 
 function Game(){
+    const apiKey = import.meta.env.VITE_APIKEY
     const [cards, setCards] = useState(cardData)
     const [seenCards, setSeenCards] = useState(new Set())
 
+    useEffect(()=>{
+        const getGifs = (async () => {
+            try{
+                var newCards = [...cards];
+                for(let i = 0; i < cards.length; i++){
+                    const card = newCards[i]
+                    if(!card.url){
+                        const response = await fetch(`https://api.giphy.com/v1/gifs/translate?api_key=${apiKey}&s=${card.val}`, {mode: 'cors'})
+                        const gifData = await response.json()
+                        card.url = gifData.data.images.original.url;
+                    }
+
+                }
+                setCards(newCards)
+            }
+            catch (error) {
+                console.log("ERRORERRORERRORERRORERRORERRORERRORERRORERRORERRORERRORERRORERRORERRORERROR", error)
+            }
+
+        })();
+    },[])
+
+    
 
     function clickCard(card){
         const cardId = card.target.dataset.id
@@ -53,7 +77,9 @@ function Game(){
         <main>
         
         {cards.map((card,index)=> {
-                return <button onClick={clickCard} className='card' data-id={card.id} key={card.id}>{card.val}</button>
+                return <div  key={"div-" + card.val}>
+                    <img onClick={clickCard} className='card' src={card.url} data-id={card.id} key={card.id}/>
+                    </div>
             })}
  
         </main>
